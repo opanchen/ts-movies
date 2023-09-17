61648e4cef945b2a7b98e221c4b4fe280689ed45
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2023-09-17 16:25:25 +0200

    Update ThemeProvider.tsx
    - connected ThemeProvider to local-storage;

--- src/components/ThemeProvider/ThemeProvider.tsx
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -1,10 +1,12 @@
 import { createContext, useState } from "react";
 
+const LS_KEY: string = "movies-ui-theme";
+
 type ThemeMode = "light" | "dark";
 
 type ThemeContextProps = {
   theme: ThemeMode;
-  setTheme: React.Dispatch<React.SetStateAction<ThemeMode>>;
+  setTheme(x: ThemeMode): void;
 };
 
 export const ThemeContext = createContext<ThemeContextProps | undefined>(
@@ -15,11 +17,23 @@ type Props = {
   children: React.ReactNode;
 };
 
+const getThemeFromStorage = (): ThemeMode => {
+  const theme: ThemeMode | null = JSON.parse(
+    localStorage.getItem(LS_KEY) || "null"
+  );
+  return theme ?? "light";
+};
+
 export const ThemeProvider = ({ children }: Props) => {
-  const [theme, setTheme] = useState<ThemeMode>("light");
+  const [mode, setMode] = useState<ThemeMode>(getThemeFromStorage);
+
+  const setTheme = (newTheme: ThemeMode) => {
+    setMode(newTheme);
+    localStorage.setItem(LS_KEY, JSON.stringify(newTheme));
+  };
 
   return (
-    <ThemeContext.Provider value={{ theme, setTheme }}>
+    <ThemeContext.Provider value={{ theme: mode, setTheme }}>
       {children}
     </ThemeContext.Provider>
   );
