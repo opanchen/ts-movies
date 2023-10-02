722f681c0f90d22722b545a964af18835e7762c1
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2023-10-02 21:15:01 +0200

    done navigation for collection page
    - implemented custom navigation for Collection page;

--- src/pages/Collection/Collection.tsx
+++ src/pages/Collection/Collection.tsx
@@ -1,15 +1,36 @@
 import { useAppSelector, useLangState } from "src/hooks";
 import { selectCollectionEn, selectCollectionUk } from "src/redux/selectors";
-import { Container, FallbackView, MovieList } from "src/components";
+import {
+  Container,
+  FallbackView,
+  LoadMoreBtn,
+  MovieList,
+} from "src/components";
 import css from "./Collection.module.css";
+import { useMemo, useState } from "react";
 
 const Collection: React.FC = () => {
   const { lang } = useLangState();
+  const [page, setPage] = useState<number>(1);
 
   const moviesEn = useAppSelector(selectCollectionEn);
   const moviesUk = useAppSelector(selectCollectionUk);
   const movies = lang === "en-US" ? moviesEn : moviesUk;
   //   console.log("EN Collection: ", movies);
+
+  const perPage: number = 20;
+  const totalPages: number = Math.ceil(movies.length / perPage);
+
+  const filteredMovies = useMemo(() => {
+    const firstIndex = 0;
+    const lastIndex = page * perPage - 1;
+    return movies.slice(firstIndex, lastIndex + 1);
+  }, [movies, page]);
+  // console.log("first index: ", firstIndex);
+
+  const handleLoadMore = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
 
   const headingTextContent: string =
     lang === "en-US" ? "Favorite movies" : "Улюблені фільми";
@@ -24,7 +45,12 @@ const Collection: React.FC = () => {
       <Container>
         <h1 className={css.heading}>{headingTextContent}</h1>
         {movies.length > 0 ? (
-          <MovieList movies={movies} />
+          <>
+            <MovieList movies={filteredMovies} />
+            {page !== totalPages && (
+              <LoadMoreBtn onLoadMore={handleLoadMore} isLoading={false} />
+            )}
+          </>
         ) : (
           <FallbackView type="init" message={errorMessage} />
         )}
